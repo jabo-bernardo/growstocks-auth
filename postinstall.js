fcd52b9105bedb63f6e283b61cd5ca67f3e211eb
Joel-Vincent <joelvincent.work@gmail.com>
2020-07-26 23:47:59 +0800

    Added postinstall

---
+++ postinstall.js
@@ -0,0 +1,13 @@
+const printMessage = require('print-message');
+
+printMessage([
+    "Welcome to GrowStocks-OAuth 🔥",
+    "---------------------------",
+    "API Developer: DataCell#4008 ✅",
+    "Original Documentation: https://medium.com/@growstocks/growstocks-oauth-38297fb6be10",
+    "---------------------------",
+    "Documentation: https://npmjs.com/package/growstocks-auth",
+    "Package Developer: https://github.com/jabo-bernardo ✅",
+    "GrowStocks: https://growstocks.xyz",
+    "---------------------------",
+])
